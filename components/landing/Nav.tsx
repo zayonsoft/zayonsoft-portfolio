2053f6347b1368e9b907d5e8865c29ebfe046b31
zayonsoft <favourlosotu@gmail.com>
2025-10-02 13:25:55 +0100

    Sidebar Toggle Working Already

--- components/landing/Nav.tsx
+++ components/landing/Nav.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { IBM_Plex_Mono } from "next/font/google";
 import { BsGithub, BsSearch, BsTwitterX } from "react-icons/bs";
@@ -7,28 +8,46 @@ import FancyMenu from "./FancyMenu";
 import { FaSearch } from "react-icons/fa";
 import { HiX } from "react-icons/hi";
 import Link from "next/link";
-import { BiCode, BiCodeAlt, BiHome, BiUser } from "react-icons/bi";
 import NavLink from "./NavLink";
 import { FiCode, FiHome, FiMonitor, FiUser } from "react-icons/fi";
-
+import { BiCopyright } from "react-icons/bi";
+import { useState } from "react";
 const ibmPlexMono = IBM_Plex_Mono({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
 export default function Nav() {
+  const [sideOpened, setSideOpened] = useState<boolean>(false);
+
+  function closeAside() {
+    setSideOpened(false);
+  }
+
+  function openAside() {
+    setSideOpened(true);
+  }
+
+  function closeAsideWhenClicked(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target == e.currentTarget) {
+      closeAside();
+    }
+  }
+
   return (
     // header deals with horizontal padding and nav deals with vertical
-    <header className="fixed left-0 right-0 px-20 max-[901px]:px-10 max-[701px]:px-4">
-      <nav className="grid grid-cols-[auto_auto] gap-8 items-center max-[701px]:grid-cols-[auto_1fr] py-5 max-[701px]:py-4 max-[701px]:border-b-2 max-[701px]:border-b-[#43454D]">
+    <header className="fixed left-0 right-0 px-20 max-[901px]:px-10 max-[701px]:px-5">
+      <nav className="grid grid-cols-[auto_auto] gap-8 items-center max-[701px]:grid-cols-[auto_1fr] py-5 max-[701px]:py-3  max-[701px]:border-b-2 max-[701px]:border-b-[#43454D]">
         <div>
-          <Image
-            className="h-auto w-33 min-w-25 max-[901px]:w-28"
-            src={"/page_logo.svg"}
-            width={120}
-            height={50}
-            alt="Logo"
-          />
+          <Link href={"/"}>
+            <Image
+              className="h-auto w-33 min-w-25 max-[901px]:w-28"
+              src={"/page_logo.svg"}
+              width={120}
+              height={50}
+              alt="Logo"
+            />
+          </Link>
         </div>
 
         {/* SEARCH AND LINKS */}
@@ -36,7 +55,7 @@ export default function Nav() {
           className={`grid w-full max-w-2xl justify-self-end grid-cols-2 max-[821px]:grid-cols-[1fr_auto] max-[701px]:grid-cols-[1fr_auto] ${ibmPlexMono.className} gap-10  max-[820px]:gap-7 items-center`}
         >
           <div className="m-auto w-full grid justify-center">
-            <search className="border-[#12F7D6] w-full px-3 py-[5px] rounded-full border-[1.5px] min-[701px]:min-w-60 max-w-73 max-[701px]:max-w-60 max-[451px]:hidden">
+            <search className="border-[#12F7D6] w-full px-3 py-[5px] rounded-full border-[1.5px] min-[701px]:min-w-60 max-w-73 max-[701px]:max-w-60 max-[500px]:hidden">
               <label
                 className="flex gap-1.5 items-center text-sm"
                 htmlFor="search"
@@ -55,19 +74,28 @@ export default function Nav() {
 
           {/* Social links */}
           <div className={`grid w-full justify-self-end`}>
-            <div className="flex justify-evenly gap-3 items-center max-[701px]:items-center text-white">
-              <span className="hidden max-[451px]:grid items-center">
-                <button className="cursor-pointer outline-none" type="button">
+            {/* MOBILE VIEW ICONS */}
+            <div className="flex justify-evenly gap-4 items-center max-[701px]:items-center text-white">
+              <span className="hidden max-[500px]:grid items-center">
+                <button
+                  className="cursor-pointer outline-none hover:bg-[#1c1c1cc4] hover:text-[#12F7D6]  p-1.5 rounded-full"
+                  type="button"
+                >
                   <FaSearch strokeWidth={1.5} />
                 </button>
               </span>
 
               <span className="hidden max-[701px]:grid items-center">
-                <button className="cursor-pointer outline-none">
+                <button
+                  className="cursor-pointer outline-none hover:bg-[#1c1c1cc4] hover:text-[#12F7D6] p-1.5 rounded-full"
+                  onClick={openAside}
+                >
                   <FancyMenu />
                 </button>
               </span>
             </div>
+            {/* MOBILE VIEW ICONS END */}
+
             <ul className="flex gap-6 justify-between max-[701px]:hidden">
               <SocialLink icon={<BsTwitterX />} name="Twitter" url="#" />
               <SocialLink icon={<FaFacebookF />} name="Facebook" url="#" />
@@ -77,7 +105,10 @@ export default function Nav() {
 
           {/* Mobile View Side bar overlay */}
           <div
-            className={`hidden max-[701px]:grid fixed left-0 right-0 top-0 h-screen bg-[#c1c1c141]`}
+            onClick={(e) => closeAsideWhenClicked(e)}
+            className={`hidden max-[701px]:grid custom-transition fixed left-0 right-0 top-0 h-screen bg-[#c1c1c12c] ${
+              !sideOpened ? "translate-x-full" : ""
+            }`}
           >
             {/* The inner div that slides in */}
             <div className="absolute grid gap-3 content-start right-0 w-11/12 max-w-80 px-4 top-0 bottom-0 bg-[#292F36]">
@@ -85,6 +116,7 @@ export default function Nav() {
                 <div className="grid items-center py-6">
                   <button
                     className="cursor-pointer text-white text-2xl"
+                    onClick={closeAside}
                     type="button"
                   >
                     <HiX />
@@ -125,16 +157,33 @@ export default function Nav() {
                   />
                 </li>
               </ul>
-              <section className="absolute grid left-0 right-0 bottom-0 px-4 text-[#12F7D6]">
-                <div className="grid border-t border-t-[#494b53] justify-center py-5">
+              <section className="absolute grid left-0 right-0 bottom-0 px-4">
+                <div className="grid justify-center  border-t border-t-[#494b53]">
+                  <p className="flex items-center gap-2 py-2 text-center text-gray-500 text-sm">
+                    <span>
+                      <BiCopyright />
+                    </span>
+                    <span>ZayonSoft 2025.</span>
+                  </p>
+                </div>
+                <div className="grid text-[#12F7D6] border-t border-t-[#494b53] justify-center py-2">
                   <div className="flex gap-5 items-center">
-                    <Link href={"#"}>
+                    <Link
+                      className="custom-transition hover:bg-black p-2 rounded-full"
+                      href={"#"}
+                    >
                       <BsTwitterX />
                     </Link>
-                    <Link href={"#"}>
+                    <Link
+                      className="custom-transition hover:bg-black p-2 rounded-full"
+                      href={"#"}
+                    >
                       <FaFacebookF />
                     </Link>
-                    <Link href={"#"}>
+                    <Link
+                      className="custom-transition hover:bg-black p-2 rounded-full"
+                      href={"#"}
+                    >
                       <BsGithub />
                     </Link>
                   </div>
