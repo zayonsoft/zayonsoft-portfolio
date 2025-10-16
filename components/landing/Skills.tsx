50623d76495998005b12ad4abca7beb4b96989b1
zayonsoft <favourlosotu@gmail.com>
2025-10-16 18:05:34 +0100

    Updated Skills Image on Mobile View

--- components/landing/Skills.tsx
+++ components/landing/Skills.tsx
@@ -5,14 +5,14 @@ import FancyUnderline from "../general/FancyUnderline";
 
 export default function Skills() {
   return (
-    <section className="bg-[#292F36] relative px-20 max-[901px]:px-10 max-[701px]:px-5 py-20 max-[701px]:py-10">
+    <section className="bg-[#292F36] relative py-20 max-[701px]:py-10">
       <div className="skills-bg absolute top-0 bottom-0 left-0 right-0 opacity-10"></div>
       <div className="relative grid gap-50">
-        <section className={`max-[701px]:hidden grid gap-25`}>
+        <section className={`grid gap-25`}>
           <div className="grid gap-10">
             <Image
               src={"/section_top_icons.svg"}
-              className="m-auto w-auto h-30"
+              className="max-[701px]:hidden m-auto w-auto h-30"
               width={100}
               height={100}
               alt="Design Icon"
@@ -26,17 +26,26 @@ export default function Skills() {
               </div>
             </div>
           </div>
-          <div>
+          <div className="max-w-full">
             <Image
               src={"/skills_image.svg"}
               height={1000}
               width={1000}
-              className="w-full h-auto max-w-[850px] m-auto"
+              className="w-full h-auto max-w-[850px] m-auto max-[701px]:hidden"
+              alt="Skills Image"
+            />
+            <Image
+              src={"/skills_image_mobile.svg"}
+              height={1000}
+              width={1000}
+              className="hidden w-full h-auto m-auto max-[701px]:block"
               alt="Skills Image"
             />
           </div>
         </section>
-        <CoreSkills />
+        <div className="px-20 max-[901px]:px-10 max-[701px]:px-5">
+          <CoreSkills />
+        </div>
       </div>
     </section>
   );
