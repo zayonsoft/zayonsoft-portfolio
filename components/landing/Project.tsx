58ab2cdcc1a030d4ee3d6140f3807a866f40e52d
zayonsoft <favourlosotu@gmail.com>
2025-10-22 17:02:42 +0100

    Made design layout for every second Project

--- components/landing/Project.tsx
+++ components/landing/Project.tsx
@@ -1,20 +1,34 @@
 import Image from "next/image";
 import { ibmPlexMono } from "./Main";
 import Link from "next/link";
-import { FiExternalLink } from "react-icons/fi";
 import { HiOutlineExternalLink } from "react-icons/hi";
-export default function Project() {
+
+type TestProps = {
+  count: number;
+};
+
+export default function Project({ count }: TestProps) {
   return (
     <section>
-      <div className="grid grid-cols-2 gap-5 py-30">
+      <div className="grid grid-cols-2 gap-5 py-30 max-[701px]:grid-cols-1">
         {/* Desc */}
-        <section className="grid items-center">
+        <section
+          className={`${
+            count % 2 == 0 ? "order-2" : ""
+          } grid items-center max-[701px]:order-2`}
+        >
           <div
             className={`grid items-center max-h-fit min-h-[300px] gap-2 ${ibmPlexMono.className}`}
           >
-            <div className="grid gap-0">
+            <div
+              className={`${
+                count % 2 == 0 ? "justify-self-end" : ""
+              } grid gap-0 max-[701px]:hidden`}
+            >
               <p
-                className={`flex text-sm items-center gap-2 text-[#12F7D6] font-medium`}
+                className={`${
+                  count % 2 == 0 ? "justify-self-end" : ""
+                } flex text-sm items-center gap-2 text-[#12F7D6] font-medium`}
               >
                 <span>Personal Project</span>
                 <span className="bg-[#12F7D6] h-1 w-1 rounded-full block"></span>
@@ -36,34 +50,48 @@ export default function Project() {
               </p>
             </div>
 
-            <div>
+            <div className={`${count % 2 == 0 ? "justify-self-end" : ""}`}>
               <Link
-                className="flex gap-1 items-center p-2.5 bg-white w-fit text-[15px] rounded-full text-[#292F36]"
+                className="flex gap-1 items-center p-2.5 bg-white w-fit text-[15px] font-medium rounded-full text-[#292F36]"
                 href={"#"}
               >
                 <span>View Project</span>
                 <span>
-                  <HiOutlineExternalLink size={22} strokeWidth={1.2} />
+                  <HiOutlineExternalLink size={22} strokeWidth={1.5} />
                 </span>
               </Link>
             </div>
           </div>
         </section>
         {/* Image Section below */}
-        <section className="relative">
-          {/* The glow at the back of the image */}
-          <div className="absolute left-0 -right-[10%] -top-[25%] -bottom-[25%] overflow">
-            <div className="absolute proj-gradient top-0 bottom-0 left-[-20%] right-[30%]"></div>
-            <div className="absolute proj-gradient top-0 bottom-0 left-0 right-[-5%]"></div>
+        <section className="grid gap-10">
+          <div className="gap-0 hidden max-[701px]:grid">
+            <p
+              className={`flex text-sm items-center gap-2 text-[#12F7D6] font-medium`}
+            >
+              <span>Personal Project</span>
+              <span className="bg-[#12F7D6] h-1 w-1 rounded-full block"></span>
+              <span className="">2024</span>
+            </p>
+            <h1 className={`text-3xl text-white tracking-wider font-medium`}>
+              CGPA Calculator
+            </h1>
           </div>
           <div className="relative">
-            <Image
-              src={"/showcase_imgs/project1.png"}
-              className="w-full border-[1.5px] border-[#12F7D6] rounded-xl m-auto"
-              alt="Project Image"
-              height={2000}
-              width={2000}
-            />
+            {/* The glow at the back of the image */}
+            <div className="absolute left-0 -right-[10%] -top-[25%] -bottom-[25%] overflow">
+              <div className="absolute proj-gradient top-0 bottom-0 left-[-20%] right-[30%]"></div>
+              <div className="absolute proj-gradient top-0 bottom-0 left-0 right-[-5%]"></div>
+            </div>
+            <div className="relative">
+              <Image
+                src={"/showcase_imgs/project1.png"}
+                className="w-full border-[1.5px] border-[#12F7D6] rounded-xl m-auto"
+                alt="Project Image"
+                height={2000}
+                width={2000}
+              />
+            </div>
           </div>
         </section>
       </div>
--- components/landing/Projects.tsx
+++ components/landing/Projects.tsx
@@ -3,7 +3,8 @@ import Project from "./Project";
 export default function Projects() {
   return (
     <section className="">
-      <Project />
+      <Project count={1} />
+      <Project count={2} />
     </section>
   );
 }
