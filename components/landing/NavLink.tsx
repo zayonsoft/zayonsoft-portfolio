902c2643b894aacb67d40c2aa5ef0d24dd2efe26
zayonsoft <favourlosotu@gmail.com>
2025-11-19 13:26:24 +0100

    Made Sidebar from Projects Page scroll not using the default id hash

--- components/landing/NavLink.tsx
+++ components/landing/NavLink.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
 
 type LinkProps = {
   id: string;
@@ -15,10 +16,25 @@ export default function NavLink({
   url,
   asideCloser,
 }: LinkProps) {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  function scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    section?.scrollIntoView();
+
+    if (pathName != "/") {
+      router.push(`/?section=${sectionId}`);
+    }
+  }
   return (
     <Link
       id={id}
-      onClick={asideCloser}
+      onClick={(e) => {
+        e.preventDefault();
+        asideCloser();
+        scrollToSection(url.slice(2));
+      }}
       className={`custom-transition outline-none flex text-md p-2 px-3 rounded-md items-center gap-3 hover:scale-95 hover:opacity-80  hover:bg-[#494b53c6] text-white bg-transparent`}
       href={url}
     >
