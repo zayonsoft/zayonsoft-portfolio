47881779abffe3126e192cb12950c261c0915290
zayonsoft <favourlosotu@gmail.com>
2025-11-22 07:40:37 +0100

    Updated sitemao.config to include projects dynamic  url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,19 @@
 module.exports = {
   siteUrl: "https://zayonsoft.vercel.app",
   generateRobotsTxt: true,
+  additionalPaths: async (config) => {
+    try {
+      const res = await fetch("https://zayonsoft.vercel.app/data/data.json");
+      const projects = await res.json();
+
+      return projects.map((project) => ({
+        loc: `/projects/${project.id}`,
+        changefreq: "weekly", // optional
+        priority: 0.7, // optional
+      }));
+    } catch (err) {
+      console.error("Error fetching projects for sitemap:", err);
+      return [];
+    }
+  },
 };
